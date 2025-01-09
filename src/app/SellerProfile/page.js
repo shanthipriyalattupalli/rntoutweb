f616a6d702792d3b5e5bca8fda4894577bfa54e2
deepak kumar <dk122809@gmail.com>
2025-01-09 12:31:19 +0000

    Updated page.js

--- src/app/SellerProfile/page.js
+++ src/app/SellerProfile/page.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState, Suspense, lazy } from "react";
 import { useParams, useSearchParams } from "next/navigation";
 import axios from "axios";
 // import "@/styles/SellerProfile.css";
-import '../../styles/SellerProfile.css'
+import '../../styles/Sellerprofile.css'
 import { useRouter } from "next/navigation";
 
 // Lazy load components
