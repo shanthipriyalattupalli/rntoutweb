8a876695d3bbc4b63cc7487da3eb544493b00fb9
shanthi <santhipriyalattupalli@gmail.com>
2025-03-06 15:59:44 +0530

    Addresssidebar changes

--- src/app/profile/manage-address/page.js
+++ src/app/profile/manage-address/page.js
@@ -1,102 +1,83 @@
 "use client";
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 // import "@/styles/Cart.css";
 import '../../../styles/Cart.css';
 import axios from "axios";
-import AddressSidebar from "../AddressSidebar/AddressSidebar"
+import AddressSidebar from "../../../Components/AddressSidebar/AddressSidebar"
 import "@/styles/Address.css";
 import { FaEllipsisV } from "react-icons/fa";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const initialAddresses = [
-  {
-    id: 1,
-    type: "Home",
-    name: "Rohan Johnson",
-    phone: "+91 1234 56789",
-    address:
-      "C/14, Vijay Apts, Old Agra Road, Naupada,next To Aradhana, Thane (w), Mumbai, Maharashtra, India - 400602",
-  },
-  {
-    id: 2,
-    type: "Office",
-    name: "Rohan Johnson",
-    phone: "+91 1234 56789",
-    address:
-      "C/14, Vijay Apts, Old Agra Road, Naupada,next To Aradhana, Thane (w), Mumbai, Maharashtra, India - 400602",
-  },
-];
 
 export default function ManageAddresses() {
   const BASE_URL = process.env.NEXT_PUBLIC_APP_BASE_URL;
-    const [isAddressSidebarOpen, setIsAddressSidebarOpen] = useState(false);
+  const [isAddressOpen, setIsAddressOpen] = useState(false);
   const [addresses, setAddresses] = useState([]);
-    const [selectedAddress, setSelectedAddress] = useState(null);
-      const [addressId, setAddressId] = useState(null);
-    
-    // const [token, setToken] = useState("");
-  
-    // useEffect(() => {
-    //   const token = localStorage.getItem("userToken");
-    //   setToken(token);
-    // }, []);
-
-    const token=(typeof window !== 'undefined') ? localStorage.getItem("userToken") : null;
 
 
-    
-useEffect(() => {
-  const fetchAddress = async () => {
-    console.log(token, "token");
-    try {
-      const response = await axios.get(`${BASE_URL}/profile/view-profile`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      console.log(response.data.profile.addresses, "addresses");
-      setAddresses(response.data.profile.addresses);
-    } catch (error) {
-      console.error(error);
-      toast.error("Failed to fetch addresses.");
+  // const [token, setToken] = useState("");
+
+  // useEffect(() => {
+  //   const token = localStorage.getItem("userToken");
+  //   setToken(token);
+  // }, []);
+
+  const token = (typeof window !== 'undefined') ? localStorage.getItem("userToken") : null;
+
+
+
+  useEffect(() => {
+    const fetchAddress = async () => {
+      console.log(token, "token");
+      try {
+        const response = await axios.get(`${BASE_URL}/profile/view-profile`, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        console.log(response.data.profile.addresses, "addresses");
+        setAddresses(response.data.profile.addresses);
+      } catch (error) {
+        console.error(error);
+        toast.error("Failed to fetch addresses.");
+      }
+    };
+
+    if (token) {
+      fetchAddress();
     }
-  };
+  }, [token]);
 
-  if (token) {
-    fetchAddress();
+
+  const initialFormData = {
+    type: "",
+    name: "",
+    mobile: "",
+    flatOrHouseNo: "",
+    street: "",
+    landmark: "",
+    city: "",
+    state: "",
+    country: "",
+    zip: "",
+    location: {
+      latitude: 0,
+      longitude: 0
+    },
   }
-}, [token]);
-
-
-const initialFormData = {
-  type: "",
-  name: "",
-  mobile: "",
-  flatOrHouseNo: "",
-  street: "",
-  landmark: "",
-  city: "",
-  state: "",
-  country: "",
-  zip: "",
-  location: {
-    latitude: 0,
-    longitude: 0
-  },
-}
 
   const [formData, setFormData] = useState(initialFormData);
   const [showForm, setShowForm] = useState(false);
 
   const handleAddressToggle = () => {
-    setIsAddressSidebarOpen(!isAddressSidebarOpen);
+    setIsAddressOpen(!isAddressOpen);
+    console.log("Address Sidebar Open:", !isAddressOpen);
   };
+
   const handleDeleteAddress = (id) => {
     setAddresses(addresses.filter((address) => address.id !== id));
   };
 
-  const handleAddress = (addressId) => {
-    setAddressId(addressId);
-  }
+
 
   return (
     <>
@@ -122,18 +103,18 @@ const initialFormData = {
             </div>
           </div>
         ))}
+        <ToastContainer />
         <button
           className='add-address-button'
           onClick={handleAddressToggle}
         >
-        Add Address 
+          Add Address
         </button>
         <AddressSidebar
-          isOpen={isAddressSidebarOpen}
+          isOpen={isAddressOpen}
           onClose={handleAddressToggle}
-          onAddressSelect={setSelectedAddress}
-          addressId={handleAddress}
         />
+
       </div>
     </>
   );
