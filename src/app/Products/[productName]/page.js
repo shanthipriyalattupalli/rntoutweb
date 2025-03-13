f6c48de415425db8f3e318b343b91d0fb3618ccc
vishanu <vishnuoffical899@gmail.com>
2025-03-13 09:48:15 +0530

    Notification responsive

--- src/app/Products/[productName]/page.js
+++ src/app/Products/[productName]/page.js
@@ -633,40 +633,48 @@ const ProductPage = () => {
               </div>
             </div>
           </>)}
-        {relatedItems.length > 3 && <div className="py-10">
-          <h2 className="font-medium text-xl">Related products</h2>
-          <div className="relative flex gap-5 pt-10">
-            <div
-              className="absolute top-1/2 transform -translate-y-1/2 z-10 cursor-pointer"
-              onClick={() => swiperRef.current?.slidePrev()} // Navigate to the previous slide
-            >
-              <img src={left} alt="Previous" className="rotate-360" />
-            </div>
-            <div
-              className="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 cursor-pointer"
-              onClick={() => swiperRef.current?.slideNext()}
-            >
-              <img src={left} alt="Next" className="rotate-180" />
-            </div>
-            <Swiper
-              onSwiper={(swiper) => (swiperRef.current = swiper)}
-              spaceBetween={20}
-              slidesPerView={4}
-              navigation={false}
-              pagination={false}
-              // pagination={{ clickable: true }}
-              modules={[Navigation, Pagination]}
-              style={{ width: '100%' }}
+        {relatedItems.length > 3 &&
+          <div className="py-10">
+            <h2 className="font-medium text-xl">Related products</h2>
+            <div className="relative flex gap-5 pt-10">
+              <div
+                className="absolute top-1/2 transform -translate-y-1/2 z-10 cursor-pointer"
+                onClick={() => swiperRef.current?.slidePrev()} // Navigate to the previous slide
+              >
+                <img src={left} alt="Previous" className="rotate-360" />
+              </div>
+              <div
+                className="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 cursor-pointer"
+                onClick={() => swiperRef.current?.slideNext()}
+              >
+                <img src={left} alt="Next" className="rotate-180" />
+              </div>
+              <Swiper
+                onSwiper={(swiper) => (swiperRef.current = swiper)}
+                spaceBetween={20}
+                slidesPerView={4} // Default for large screens
+                navigation={false}
+                pagination={false}
+                modules={[Navigation, Pagination]}
+                style={{ width: '100%' }}
+                breakpoints={{
+                  320: { slidesPerView: 1 }, 
+                  375: { slidesPerView: 1 },
+                  425: { slidesPerView: 2 }, 
+                  768: { slidesPerView: 3 }, 
+                  1024: { slidesPerView: 4 }, 
+                }}
+              >
+                {relatedItems.map((product) => (
+                  <SwiperSlide key={product._id} className="flex justify-center">
+                    <ProductItem product={product} />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
 
-            >
-              {relatedItems.map((product) => (
-                <SwiperSlide key={product._id} className="flex justify-center">
-                  <ProductItem product={product} />
-                </SwiperSlide>
-              ))}
-            </Swiper>
+            </div>
           </div>
-        </div>}
+        }
       </div>
 
     </>
