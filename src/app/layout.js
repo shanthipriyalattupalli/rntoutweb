a30573c02ed392d762c1f781ac89e3d2106c7d33
tsdeepak-123 <tsdeepak589@gmail.com>
2025-08-02 14:00:26 +0530

    ok

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,36 +1,16 @@
-// app/layout.js
+// app/layout.js (Clean and organized)
 import "./globals.css";
 import Header from "@/Components/Layout/Header";
 import Newsletter from "@/Components/Layout/Newsletter";
 import MobileApp from "@/Components/Home/MobileApp";
 import GoogleMapsProvider from "../Components/Location/GoogleMapsProvider";
 import GlobalLoading from "@/Components/GlobalLoading";
-import Script from 'next/script';
+import TrackingProvider from "@/Tracking/index";
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <head>
-        {/* Noscript fallbacks */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=30444831931827949&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
-        
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-595VNGRT"
-            height="0" 
-            width="0" 
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
       </head>
       <body className="antialiased">
         <GlobalLoading />
@@ -43,73 +23,8 @@ export default function RootLayout({ children }) {
           <Newsletter />
         </GoogleMapsProvider>
         
-        {/* Google Analytics (gtag.js) */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-J70JW4R835"
-          strategy="afterInteractive"
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-J70JW4R835');
-            `
-          }}
-        />
-        
-        {/* Google Tag Manager */}
-        <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-595VNGRT');
-            `
-          }}
-        />
-        
-        {/* Microsoft Clarity */}
-        <Script
-          id="microsoft-clarity"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "sc0wg6wpgr");
-            `
-          }}
-        />
-        
-        {/* Meta Pixel Script */}
-        <Script
-          id="meta-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '30444831931827949');
-              fbq('track', 'PageView');
-            `
-          }}
-        />
+        {/* All tracking scripts in one clean component */}
+        <TrackingProvider />
       </body>
     </html>
   );
